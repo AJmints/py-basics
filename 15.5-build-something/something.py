13f834c571d9aefed0387df79eaffa9d9ec5a602
ajmints <alexjmints@gmail.com>
2023-11-26 14:46:48 -0600

    wsl wins...

--- 15.5-build-something/something.py
+++ 15.5-build-something/something.py
@@ -2,15 +2,18 @@
 import sys
 import random
 
+
 def something():
+
     print("something")
-    randomNum = random.choice("123456789")
-    print(randomNum)
-    if randomNum > 5:
+    random_num = random.choice("123456789")
+    print(random_num)
+    if random_num > 5:
         print("Nice")
-    elif randomNum < 3:
+    elif random_num < 3:
         print("Also nice...")
     else:
         print("4 or 5, less than 6")
+
 
 something()
