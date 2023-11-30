9e3a50a46f4371ca952faa736be0c717cfb61c48
ajmints <alexjmints@gmail.com>
2023-11-29 19:29:42 -0600

    learn venv, but for vsc, different for pycharm it seems

---
+++ 20-pip-venv/pip.py
@@ -0,0 +1,10 @@
+# python3 -m pip install request  # Linux Mac
+# py -m pip install request  # Windows
+
+# above is for vsc, it is done differently for pycharm
+
+# Learning about virtual environments. Getting an overview.
+
+import pip
+
+# Starting up virtual environment with pip is not working yet...
